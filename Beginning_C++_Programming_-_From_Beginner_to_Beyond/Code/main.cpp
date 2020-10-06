4589197c935f5ec8df5a19f0df6ef37d1f43ca1e
DavidCorzo <davidcorzo@ufm.edu>
2020-10-05 22:52:50 -0600

    2020-10-05

--- Beginning_C++_Programming_-_From_Beginner_to_Beyond/Code/main.cpp
+++ Beginning_C++_Programming_-_From_Beginner_to_Beyond/Code/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 
-using namespace std;
-
-int main(){
-    
+int main() {
+    int favorite_number;
+    std::cout << "Enter your favorite number between 1 and 100: ";
+    std::cin >> favorite_number;
+    std::cout << "Amazing!! Thats my favorite number too!" << std::endl;
+    std::cout << "No really!!, " << favorite_number << " is my favorite number!" << std::endl;
 }
