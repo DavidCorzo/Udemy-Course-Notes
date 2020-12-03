4ed33516364d81d741abda1d13a14ebda46ea888
DavidCorzo <davidcorzo@ufm.edu>
2020-12-02 23:57:23 -0600

    2020-12-2

--- Udemy/Beginning_C++_Programming_-_From_Beginner_to_Beyond/Code/main.cpp
+++ Udemy/Beginning_C++_Programming_-_From_Beginner_to_Beyond/Code/main.cpp
@@ -1,35 +1,15 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int score {};
-    cout << "Enter your score in the exam (0-100): ";
-    cin >> score;
-    char letter_grade {};
-
-    if (score >= 0 && score <= 100) {
-        if (score >= 90)
-            letter_grade = 'A';
-        else if (score >= 80)
-            letter_grade = 'B';
-        else if (score >= 70)
-            letter_grade = 'C';
-        else if (score >= 60)
-            letter_grade = 'D';
-        else
-            letter_grade = 'F';
-        cout << "Your grade is: " << letter_grade << endl;
-        if (letter_grade == 'F') 
-            cout << "Bad grade." << endl;
-        else 
-            cout << "Congrats." << endl;
-    } else {
-        cout << "Sorry, " << score << "is not in range." << endl;
+    string str;
+    getline(cin, str);
+    size_t length {str.length()};
+    char line[2*str.length()-1] {};
+    for (auto c : str ) {
+        line[length] = c;
     }
     return 0;
 }
 /* OUTPUT:
-Enter your score in the exam (0-100): 90
-Your grade is: A
-Congrats.
 
 */
