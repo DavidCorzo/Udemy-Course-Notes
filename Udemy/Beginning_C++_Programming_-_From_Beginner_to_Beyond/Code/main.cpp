d157ac723423b5802fa6bc5ca480614bcfc537a8
DavidCorzo <davidcorzo@ufm.edu>
2021-01-14 22:56:04 -0600

    2021-01-14

--- Udemy/Beginning_C++_Programming_-_From_Beginner_to_Beyond/Code/main.cpp
+++ Udemy/Beginning_C++_Programming_-_From_Beginner_to_Beyond/Code/main.cpp
@@ -1,17 +1,5 @@
 #include <iostream>
 using namespace std;
-
-int *dont_do_this() {
-    int size {};
-    return &size;
-}
-
-int *or_this() {
-    int size {};
-    int *int_ptr {&size};
-    return int_ptr;
-}
-
 int main() {
     
     return 0;
